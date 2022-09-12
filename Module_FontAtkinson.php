d96d7cae16d9ac238b4fb62705a234db894ab3d0
gizmore <gizmore@wechall.net>
2022-09-12 08:06:26 +0200

    '7.0.1-r1591 font atkinson'

---
+++ Module_FontAtkinson.php
@@ -0,0 +1,31 @@
+<?php
+namespace GDO\FontAtkinson;
+
+use GDO\Core\GDO_Module;
+use GDO\Core\GDT_Checkbox;
+
+final class Module_FontAtkinson extends GDO_Module
+{
+	public string $license = 'Atkinson';
+	
+	public function getConfig() : array
+	{
+		return [
+			GDT_Checkbox::make('use_globally')->initial('1'),
+		];
+	}
+	public function cfgUseGlobally() : bool { return $this->getConfigValue('use_globally'); }
+	
+	public function onLoadLanguage() : void { $this->loadLanguage('lang/atkinson'); }
+	
+	public function onIncludeScripts() : void
+	{
+		$this->addCSS('css/atkinson.css');
+		if ($this->cfgUseGlobally())
+		{
+			$this->addCSS('css/use_atkinson.css');
+		}
+	}
+	
+
+}
---
+++ lang/atkinson_en.php
@@ -0,0 +1,5 @@
+<?php
+namespace GDO\FontAtkinson\lang;
+return [
+	'cfg_use_globally' => 'Use font for eveything',
+];
